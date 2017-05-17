bb70ea8466d1f5c92b0128e9100163535f4dc3a8
Samaksh Jain <samakshjain@live.com>
2017-05-17 07:39:14 +0530

    Add soln to merge_intervals

--- array/merge_intervals.py
+++ array/merge_intervals.py
@@ -6,11 +6,13 @@ Given [1,3],[2,6],[8,10],[15,18],
 return [1,6],[8,10],[15,18].
 """
 
+
 # Definition for an interval.
 class Interval(object):
     def __init__(self, s=0, e=0):
         self.start = s
         self.end = e
+
 
 def merge(intervals):
     """
@@ -25,16 +27,31 @@ def merge(intervals):
             out += i,
     return out
 
+
 def print_intervals(intervals):
     res = []
     for i in intervals:
-        res.append('['+str(i.start)+','+str(i.end)+']')
+        res.append('[' + str(i.start) + ',' + str(i.end) + ']')
     print("".join(res))
 
+
+# >>>>>>>
+
+def merge_overlapping(intervals):
+    final = [intervals[0]]
+    for idx, interval in enumerate(intervals[1:]):
+        if final[-1][1] > interval[0]:
+            final[-1] = [final[-1][0], interval[1]]
+        else:
+            final.append(interval)
+    return final
+
+
 if __name__ == "__main__":
-    given = [[1,3],[2,6],[8,10],[15,18]]
+    given = [[1, 3], [2, 6], [8, 10], [15, 18]]
     intervals = []
     for l, r in given:
-        intervals.append(Interval(l,r))
+        intervals.append(Interval(l, r))
     print_intervals(intervals)
     print_intervals(merge(intervals))
+    print merge_overlapping(given)
